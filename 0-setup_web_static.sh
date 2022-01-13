ed1c57c03e438c2d3056f37f82938259410ad084
EshietIy <eshietinyang@gmail.com>
2022-01-13 16:37:06 +0100

    bash that sets up web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,14 @@
-#/usr/bin/env bash
-# A script that sets up web server for deployment
-FILE=/etc/nginx/sites_available/defult
-STRING="location  /hbnb_static/ {\n alias /data/web_static/current/; \n }\n"
+#!/usr/bin/env bash
+# Sets up server for the deployment of web_static
+FILE=/etc/nginx/sites-available/default
+STRING="location /hbnb_static/ {\n alias /data/web_static/current/; \n}\n"
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p  /data/web_static/shared/
-echo "software Engineering is fun" > /data/web_static/releases/test/index.html
-sudo ln -sf data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/shared
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "39i $STRING" $FILE
-sudo service nginix restart
+sudo service nginx restart
