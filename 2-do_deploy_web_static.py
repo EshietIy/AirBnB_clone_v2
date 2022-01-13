1ce5ccb7457d6e3faedc7c5eee4f1359bffd8463
EshietIy <eshietinyang@gmail.com>
2022-01-13 21:51:59 +0100

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,19 @@ import os
 
 env.hosts = ['35.190.182.223', '18.232.186.120']
 
+def do_pack():
+    """archives the static file"""
+    try:
+        filepath = "versions/web_static_" + datetime.now().\
+            strftime("%Y%m%d%H%M%S") + ".tgz"
+        local("mkdir -p versions")
+        local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
+     web_static")
+        print("web_static packed: {} -> {}".
+              format(filepath, os.path.getsize(filepath)))
+    except BaseException:
+        return None
+
 def do_deploy(archive_path):
     """deploy archive to server"""
     a_list = archive_path.split(".tgz")
