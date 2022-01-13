98171a882f7baf3f6989bf86d5029b7cf7f88aa2
EshietIy <eshietinyang@gmail.com>
2022-01-13 18:07:01 +0100

    do pack function

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,11 +8,12 @@ from datetime import datetime
 def do_pack():
     """archives the static file"""
     try:
-        filepath = "versions/web_static_" + datetime.now().\
+        filepath = "versions/web_static_" + datetime.now().
         strftime("%Y%m%d%H%M%S") + ".tgz"
         local("mkdir -p versions")
         local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
         web_static")
-        print("web_static packed: {} -> {}".format(filepath, os.path.getsize(filepath)))
+        print("web_static packed: {} -> {}".
+              format(filepath, os.path.getsize(filepath)))
     except BaseException:
         return None
