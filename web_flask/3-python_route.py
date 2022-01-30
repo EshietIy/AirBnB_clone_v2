93e58e0d257168c702360b5be613f4f87228cfdc
EshietIy <eshietinyang@gmail.com>
2022-01-30 02:35:18 +0100

    return variable with default text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''Defines a Flask App'''
+from flask import Flask
+
+
+app = Flask('__name__')
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """return string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """return string"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """retrun variable 'text' """
+    return 'C {:s}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python_text(text):
+    """ return variable with default text"""
+    return 'Python {:s}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
