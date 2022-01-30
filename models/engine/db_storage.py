6a4c4df4a917c8a543a9dc2234047b4985b31106
EshietIy <eshietinyang@gmail.com>
2022-01-30 15:12:52 +0100

    fixed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -86,7 +86,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
-        self.__session = scoped_session(session_factory)()
+        self.__session = scoped_session(session_factory)
 
     def close(self):
         '''closing current session'''
